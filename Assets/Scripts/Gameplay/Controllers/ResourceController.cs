11054017ba3135705b27cf4e039dec9bfda38c60
z.terehov <zaharkuli67@gmail.com>
2023-10-11 00:00:36 +0300

    Added resource controller

---
+++ Assets/Scripts/Gameplay/Controllers/ResourceController.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+namespace Gameplay.Controllers
+{
+    public class ResourceController : MonoBehaviour
+    {
+        [SerializeField] float _interval = 2f;
+        
+        [SerializeField] private float _unitsCapital;
+        [SerializeField] private float _unitsPerSecond = 1f;
+        
+        [Space]
+        [SerializeField] private int _gold;
+        [SerializeField] private int _goldPerSecond = 1;
+        
+        [Space]
+        [SerializeField] private float _unitRecruitmentRate = 1.1f;
+        
+        private float _time;
+
+        private void Start()
+        {
+            _time = 0f;
+        }
+        
+        private void Update() 
+        {
+            _time += Time.deltaTime;
+            
+            while(_time >= _interval) {
+                ResourceProduction();
+                _time -= _interval;
+            }
+        }
+
+        private void ResourceProduction()
+        {
+            _unitsCapital += _unitsPerSecond * _unitRecruitmentRate;
+            _gold += _goldPerSecond;
+        }
+    }
+}
